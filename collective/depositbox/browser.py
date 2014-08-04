ddf91661387ccd513dfcb901a6a0b174ccc54eff
Maurits van Rees <maurits@vanrees.org>
2014-08-04 14:55:59 +0200

    Add deposit-box-data.csv download view.

--- collective/depositbox/browser.py
+++ collective/depositbox/browser.py
@@ -1,7 +1,9 @@
+import csv
 from AccessControl import Unauthorized
 from Acquisition import aq_inner
 from Products.Five import BrowserView
 from Products.CMFCore.utils import _checkPermission
+from StringIO import StringIO
 from collective.depositbox.interfaces import IDepositBox
 
 
@@ -10,6 +12,12 @@ class DepositBoxView(BrowserView):
 
     Simply a front end for the adapter really.
     """
+
+    # Character set to use for exporting to csv.  Excel likes
+    # iso-8859-1: when you use 'utf-8' as export charset excel will
+    # show wrong characters for names with c-cedille or other such
+    # characters.  So we want to send iso-8859-1 here.
+    export_charset = 'iso-8859-1'
 
     def put(self, value, token=None):
         context = aq_inner(self.context)
@@ -48,3 +56,24 @@ class DepositBoxView(BrowserView):
             return []
         box = IDepositBox(context)
         return box.get_all_confirmed()
+
+    def confirmed_as_csv(self):
+        out = StringIO()
+        writer = csv.writer(out, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for values in self.get_all_confirmed():
+            print values
+            writer.writerow(values)
+        response = self.request.response
+        response.setHeader('content-type',
+                           'application/vnd.ms-excel; charset=%s' %
+                           self.export_charset)
+        filename = 'deposit-box-data.csv'
+        response.setHeader('content-disposition',
+                           'attachment; filename=%s' % filename)
+
+        # Some values may have characters that cannot be translated
+        # into the chosen charset, like \u2018 which Microsoft is so
+        # fond of...  So replace faulty characters with a question
+        # mark.
+        return out.getvalue().encode(self.export_charset, 'replace')
